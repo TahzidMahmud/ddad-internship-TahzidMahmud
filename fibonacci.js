c8f6144f0e82d2121fd8fedf704f5a0bfadf8167
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-01-15 09:45:29 +0600

    slight logic modification for fibonacci since some test cases failed due to the logic

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,21 @@
-const fib = [0, 1, 1];
+let fib = [];
 const fibonacci = (x, memo = {}) => {
-  if (x <= 20 && x >= -1) {
+  if (x <= 20) {
     if (x in memo) {
       return memo[x];
     }
-    if (x <= 2) {
+    if (x == 0) {
+      memo[x] = 0;
+      fib.push(0);
+      return 0;
+    }
+    if (x == 1) {
+      memo[x] = 1;
+      fib.push(1);
       return 1;
     }
     memo[x] = fibonacci(x - 1, memo) + fibonacci(x - 2, memo);
-    // console.log(memo[x]);
+
     fib.push(memo[x]);
     return memo[x];
   } else {
@@ -16,14 +23,15 @@ const fibonacci = (x, memo = {}) => {
   }
 };
 
-// fib.forEach((num) => console.log(num));
-
 const calculate_fibonacci = () => {
   const x = document.getElementById("fibonacci").value;
   fibonacci(x);
   const res_div = document.getElementById("fibonacci_Result");
-  for (let i = 0; i < x; i++) {
-    console.log(fib[i]);
-    res_div.innerHTML = `<br>Required Fibonacci Series: ${fib}`;
-  }
+
+  fib = [...fib].sort((a, b) => a - b);
+  fib.pop();
+  res_div.innerHTML = `<br>Required Fibonacci Series: ${fib}`;
+
+  console.log(fib);
+  fib = [];
 };
